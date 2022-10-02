01516a249de123f5be88247bf31b92a5629480db
Lakshmisureshan <113295897+Lakshmisureshan@users.noreply.github.com>
2022-10-02 08:28:35 +0400

    1 file modified

--- src/Review.js
+++ src/Review.js
@@ -4,6 +4,11 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+  const checknumber = (number) => {
+if (number >people.length-1)
+
+
+  };
   return <article className="review"></article>;
 };
 export default Review;
