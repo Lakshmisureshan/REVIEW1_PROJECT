614422935aec3e0273a7e0e5b775887edd7016b7
Lakshmisureshan <113295897+Lakshmisureshan@users.noreply.github.com>
2022-10-02 08:34:07 +0400

    1 file modified

--- src/Review.js
+++ src/Review.js
@@ -5,9 +5,19 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
   const checknumber = (number) => {
-if (number >people.length-1)
+    if (number > people.length - 1) return 0;
 
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
 
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
   };
   return <article className="review"></article>;
 };
